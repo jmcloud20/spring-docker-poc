7c4b09509d180fe28b82949254dd55b6bb1a434e
81255820 <81255820@corp.root>
2019-11-21 12:49:21 +0800

    Additional modified files during testing

--- src/main/java/com/pccw/springdockerpoc/TomcatController.java
+++ src/main/java/com/pccw/springdockerpoc/TomcatController.java
@@ -4,17 +4,40 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TomcatController{
+
+    public static final String CONST_BLANK = "";
+
+    @Value("${db.uname}")
+    public String dbUname;
+
+    @Value("${db.pword}")
+    public String dbPword;
 
     @GetMapping("/hello")
     public Collection<String> sayHello(){
         return IntStream.range(0, 10)
             .mapToObj(i -> "Hello number " + i)
             .collect(Collectors.toList());
+    }
+
+    @GetMapping("/properties")
+    public String getProperties(@RequestParam(required = false) String propertyName){
+        String value = TomcatController.CONST_BLANK;
+        propertyName = propertyName == null ? TomcatController.CONST_BLANK : propertyName;
+
+        switch(propertyName){
+            case "dbuname": value = this.dbUname; break;
+            case "dbpword": value = this.dbPword; break;
+            default: value = "No value for this property.";
+        }
+        return value;
     }
 
 }
